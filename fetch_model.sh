699322ba721bb53f7c836cac5db224bdbb5a13f2
gboxo <gboxocor7@alumnes.ub.edu>
2023-08-10 16:18:36 +0200

    Initial Commit

---
+++ fetch_model.sh
@@ -0,0 +1,2 @@
+wget -O pytorch_model.bin https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/resolve/main/pytorch_model.bin
+wget -O vocab.txt wget https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/blob/main/vocab.txt
